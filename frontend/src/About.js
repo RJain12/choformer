f8a1264e3fb6adde6d7b4f79f90bb2f383d26bee
Dhruvsr <dhruvramu@gmail.com>
2024-10-11 17:49:54 +0530

    [frontend] App.js and skeletal structure

---
+++ frontend/src/About.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import styled, { keyframes } from 'styled-components';
+import ncbiLogo from './assets/ncbi.png'; // Adjust the path as necessary
+import openaiLogo from './assets/openai.png'; // Adjust the path as necessary
+import awsLogo from './assets/aws.png'; // Adjust the path as necessary
+
---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Tool from './Tool';
+import CHOFormer from './CHOFormer';
+import CHOExp from './CHOExp';
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/CHOFormer" element={<CHOFormer />} />
+                <Route path = "/CHOExp" element={<CHOExp/>}/>
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
