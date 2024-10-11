344a1932cdea759bef752151ecf92c3acbf37a54
Dhruvsr <dhruvramu@gmail.com>
2024-10-11 17:44:28 +0530

    [frontend] initiating react project

--- data_preprocessing/download.R
+++ data_preprocessing/download.R
@@ -1,19 +1,15 @@
 library(httr)
 library(jsonlite)
 
-# Define the base URL for the NCBI datasets API
 base_url <- "https://api.ncbi.nlm.nih.gov/datasets/v1/genome/taxon/10029/download"
 
-# Make an API request to download the genomic data
 response <- GET(base_url, query = list(
-  refseq = "true",  # Reference genomes only
-  format = "fasta",  # Download in FASTA format
+  refseq = "true", 
+  format = "fasta",  
   filename = "cricetulus_genome.zip"  # Specify output filename
 ))
 
-# Check if the request was successful
 if (status_code(response) == 200) {
-  # Write the content to a ZIP file
   writeBin(content(response, "raw"), "cricetulus_genome.zip")
   cat("Download complete. File saved as 'cricetulus_genome.zip'.\n")
 } else {
